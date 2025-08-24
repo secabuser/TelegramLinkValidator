5b4f1ae540683bb11af67dfc1390e3c9c30f1150
r.gh <hiradsec@gmail.com>
2025-08-24 12:44:16 +0330

    Update gen.py

--- gen.py
+++ gen.py
@@ -7,60 +7,60 @@ from time import sleep
 
 init(autoreset=True)
 
-G = Fore.GREEN
-R = Fore.RED
-W = Fore.WHITE
-RE = Fore.RESET
+_g = Fore.GREEN
+_r = Fore.RED
+_w = Fore.WHITE
+_re = Fore.RESET
 
-class G:
+class _g_class:
     def __init__(self):
-        self.f = "link.txt"
+        self._f = "link.txt"
 
-    def clr(self):
+    def _c(self):
         os.system("cls" if os.name == "nt" else "clear")
 
-    def bnr(self):
-        b = '''
+    def _b(self):
+        _n = '''
 
-▗▖   ▗▄▄▄▖▗▖  ▗▖▗▖ ▗▖     ▗▄▄▖▗▄▄▄▖▗▖  ▗▖
-▐▌     █  ▐▛▚▖▐▌▐▌▗▞▘    ▐▌   ▐▌   ▐▛▚▖▐▌
-▐▌     █  ▐▌ ▝▜▌▐▛▚▖     ▐▌▝▜▌▐▛▀▀▘▐▌ ▝▜▌
-▐▙▄▄▖▗▄█▄▖▐▌  ▐▌▐▌ ▐▌    ▝▚▄▞▘▐▙▄▄▖▐▌  ▐▌
-                                                                  
-              t.me/secabuser 
+▗▖   ▗▄▄▄▖▗▖  ▗▖▗▖ ▗▖     ▗▄▄▖▗▄▄▄▖▗▖  ▗▖
+▐▌     █  ▐▛▚▖▐▌▐▌▗▞▘    ▐▌   ▐▌   ▐▛▚▖▐▌
+▐▌     █  ▐▌ ▝▜▌▐▛▚▖     ▐▌▝▜▌▐▛▀▀▘▐▌ ▝▜▌
+▐▙▄▄▖▗▄█▄▖▐▌  ▐▌▐▌ ▐▌    ▝▚▄▞▘▐▙▄▄▖▐▌  ▐▌
+                                                                  
+              t.me/secabuser 
 '''
-        print(Colorate.Diagonal(Colors.red_to_blue, Center.XCenter(b)))
+        print(Colorate.Diagonal(Colors.red_to_blue, Center.XCenter(_n)))
 
-    def code(self, l=16):
-        c = string.ascii_letters + string.digits
-        return ''.join(random.choices(c, k=l))
+    def _co(self, _l=16):
+        _c = string.ascii_letters + string.digits
+        return ''.join(random.choices(_c, k=_l))
 
-    def gen(self, n):
-        return [f"https://t.me/+{self.code()}" for _ in range(n)]
+    def _ge(self, _n):
+        return [f"https://t.me/+{self._co()}" for _ in range(_n)]
 
-    def save(self, lst):
-        if os.path.exists(self.f):
-            os.remove(self.f)
-        with open(self.f, "w") as f:
-            for i in lst:
-                f.write(i + "\n")
-        print(f"\n{G}[!]{RE} Saved {len(lst)} links to {W}{self.f}{RE}\n")
+    def _s(self, _l):
+        if os.path.exists(self._f):
+            os.remove(self._f)
+        with open(self._f, "w") as _f:
+            for _i in _l:
+                _f.write(_i + "\n")
+        print(f"\n{_g}[!]{_re} Saved {len(_l)} links to {_w}{self._f}{_re}\n")
 
 if __name__ == "__main__":
-    g = G()
-    g.clr()
-    g.bnr()
+    _a = _g_class()
+    _a._c()
+    _a._b()
 
     try:
-        n = int(input(f"{W}Number > {RE}").strip())
+        _n = int(input(f"{_w}Number > {_re}").strip())
     except:
-        print(f"{R}[!]{RE} Invalid number !")
+        print(f"{_r}[!]{_re} Invalid number !")
         exit(1)
 
-    g.clr()
-    g.bnr()
+    _a._c()
+    _a._b()
 
-    print(f"{W}[~] Generating {n} links...{RE}")
-    lst = g.gen(n)
-    g.save(lst)
+    print(f"{_w}[~] Generating {_n} links...{_re}")
+    _l = _a._ge(_n)
+    _a._s(_l)
     sleep(1)
